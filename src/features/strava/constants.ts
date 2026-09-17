f86d5f60d35a300ef15220d3f89923caf0becd4b
jayidoans <jay.sihotang@unai.edu>
2026-09-17 17:14:57 +0700

    dumb me

--- src/features/strava/constants.ts
+++ src/features/strava/constants.ts
@@ -1,7 +1,7 @@
 export const STRAVA_AUTHORIZE_URL = "https://www.strava.com/oauth/authorize";
 export const STRAVA_TOKEN_URL = "https://www.strava.com/oauth/token";
 export const STRAVA_REVOKE_URL = "https://www.strava.com/oauth/revoke";
-export const STRAVA_API_BASE_URL = "https://api-v3.strava.com";
+export const STRAVA_API_BASE_URL = "https://www.strava.com/api/v3";
 
 export const STRAVA_REQUIRED_SCOPES = ["read", "activity:read_all"] as const;
 export const STRAVA_STATE_TTL_SECONDS = 10 * 60;
